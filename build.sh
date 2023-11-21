c1cf70fea75b64034d00d89f058e0a837e78e790
whatev33r <113145908+whatev33r@users.noreply.github.com>
2023-11-21 13:50:46 +0100

    add build.sh

---
+++ build.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# var
+destiny="$PWD/x86_64"
+
+# spawn folders
+tput setaf 10
+echo "> Cleaning /tmp/ctos-build"
+tput sgr0
+
+rm -rf /tmp/ctos-build
+mkdir /tmp/ctos-build
+export CHROOT=/tmp/ctos-build
+
+# generate chroot
+tput setaf 10
+echo "> Setting up chroot env in $CHROOT"
+tput sgr0
+
+mkarchroot -M configs/makepkg.conf -C configs/pacman.conf $CHROOT/root base-devel
+arch-nspawn $CHROOT/root pacman -Syu &> /dev/null
+
+# build pkgs
+tput setaf 10
+echo "> Building pkgs"
+tput sgr0
+
+for d in packages/*; do
+  echo Building $d..
+  cd $d
+  makechrootpkg -r $CHROOT
+  cd ../../
+done
+
+# clean folder & mv pkgs
+tput setaf 10
+echo "> Cleaning folders"
+tput sgr0
+
+for d in packages/*; do
+  echo Cleaning $d..
+  cd $d
+  # mv pkgs
+  mv -n *pkg.tar.zst $destiny &> /dev/null
+  mv -n *pkg.tar.zst.sig $destiny &> /dev/null
+  # clean folder
+  if [[ -f $wpdpath/*.log ]]; then
+    rm $pwdpath/*.log
+  fi
+  if [[ -f $wpdpath/*.deb ]]; then
+    rm $pwdpath/*.deb
+  fi
+  if [[ -f $wpdpath/*.tar.gz ]]; then
+    rm $pwdpath/*.tar.gz
+  fi
+  cd ../../
+done
+
+# generate repo db
+tput setaf 10
+echo "> Generating repo database"
+tput sgr0
+
+repo-add $destiny/ctOS.db.tar.gz $destiny/*.pkg.tar.zst
